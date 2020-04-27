44fe7a49fa9ddf2d9983d016e1d37f013e16b980
Manpreeth Kaur <manpreeth.kaur@hitachivantara.com>
2020-04-28 01:17:30 +0530

    LDNG-4108 Remove Sonar Analised error from Global Footer Model and Code cleaning

--- core/src/main/java/com/toyota/tmna/lexusdrivers/core/models/DisplayMessageModel.java
+++ core/src/main/java/com/toyota/tmna/lexusdrivers/core/models/DisplayMessageModel.java
@@ -2,7 +2,10 @@ package com.toyota.tmna.lexusdrivers.core.models;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
+
 import javax.annotation.Nonnull;
+
+import com.toyota.tmna.lexusdrivers.core.util.ConstantsUtil;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
@@ -10,60 +13,60 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 @Model(adaptables = SlingHttpServletRequest.class,
-    resourceType = DisplayMessageModel.RESOURCE_TYPE,
-    adapters = {DisplayMessageModel.class, ComponentExporter.class},
-    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+        resourceType = DisplayMessageModel.RESOURCE_TYPE,
+        adapters = {DisplayMessageModel.class, ComponentExporter.class},
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class DisplayMessageModel implements ComponentExporter {
 
-  protected static final String RESOURCE_TYPE = "lexusdrivers/components/content/displayMessage";
+    protected static final String RESOURCE_TYPE = ConstantsUtil.DISPLAY_MESSAGE_RESOURCE;
 
-  @ValueMapValue(name = "primaryButtonText")
-  private String primaryButtonText;
+    @ValueMapValue(name = "primaryButtonText")
+    private String primaryButtonText;
 
-  @ValueMapValue(name = "primaryButtonLink")
-  private String primaryButtonLink;
+    @ValueMapValue(name = "primaryButtonLink")
+    private String primaryButtonLink;
 
-  @ValueMapValue(name = "description")
-  private String description;
+    @ValueMapValue(name = "description")
+    private String description;
 
-  @ValueMapValue(name = "verticalCenterAlign")
-  private String verticalCenterAlign;
+    @ValueMapValue(name = "verticalCenterAlign")
+    private String verticalCenterAlign;
 
-  @ValueMapValue(name = "supportContent")
-  private String supportContent;
+    @ValueMapValue(name = "supportContent")
+    private String supportContent;
 
-  @ValueMapValue(name = "title")
-  private String title;
+    @ValueMapValue(name = "title")
+    private String title;
 
 
-  public String getPrimaryButtonText() {
-    return primaryButtonText;
-  }
+    public String getPrimaryButtonText() {
+        return primaryButtonText;
+    }
 
-  public String getPrimaryButtonLink() {
-    return primaryButtonLink;
-  }
+    public String getPrimaryButtonLink() {
+        return primaryButtonLink;
+    }
 
-  public String getDescription() {
-    return description;
-  }
+    public String getDescription() {
+        return description;
+    }
 
-  public String getVerticalCenterAlign() {
-    return verticalCenterAlign;
-  }
+    public String getVerticalCenterAlign() {
+        return verticalCenterAlign;
+    }
 
-  public String getSupportContent() {
-    return supportContent;
-  }
+    public String getSupportContent() {
+        return supportContent;
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  @Nonnull
-  @Override
-  public String getExportedType() {
-    return RESOURCE_TYPE;
-  }
+    @Nonnull
+    @Override
+    public String getExportedType() {
+        return RESOURCE_TYPE;
+    }
 }
