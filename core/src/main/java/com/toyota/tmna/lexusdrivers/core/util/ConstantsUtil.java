ef5869d570ed0f6c72bcb19282f0bc5603171381
Shashank Arcot <shashank.arcot@toyota.com>
2020-05-03 23:55:58 +0530

    LDNG-4248 Security Fixes and gitifnore fixes

--- core/src/main/java/com/toyota/tmna/lexusdrivers/core/util/ConstantsUtil.java
+++ core/src/main/java/com/toyota/tmna/lexusdrivers/core/util/ConstantsUtil.java
@@ -23,7 +23,7 @@ public class ConstantsUtil {
 
     /* Forgot Password Message*/
 
-    public static final String FORGOT_PASSWORD_RESOURCE = "lexusdrivers/components/content/forgotPassword";
+    public static final String FORGOTPWD_RESOURCE_FOLDER = "lexusdrivers/components/content/"; //HF SA:03/05/2020 Security issue
 
     /* Global Navigation Message*/
 
