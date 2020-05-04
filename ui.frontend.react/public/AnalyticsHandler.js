88ef100ba8fe0914bd694721e303e975211ebe44
Hussain Shaik <hussain.shaik@toyota.com>
2020-05-04 09:52:13 +0530

    LDNG-4122 Footer console error

--- ui.frontend.react/public/AnalyticsHandler.js
+++ ui.frontend.react/public/AnalyticsHandler.js
@@ -24,7 +24,7 @@ var tagCall = function (tagKey, tagContentTemplate) {
 }
 var fireTagCall = function (tagKey, errorMsg) {
     var tagContentTemplate = jQuery.extend(true, {}, '');
-    if (fireTagParam.length != 0) {
+    if (fireTagParam != null && fireTagParam.length != 0) {
         $.extend(true, tagContentTemplate, fireTagParam);
         $.extend(true, tagContentTemplate, digitalData.page);
     }
