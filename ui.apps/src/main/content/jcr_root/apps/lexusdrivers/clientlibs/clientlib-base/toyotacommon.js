b217eacf84f1dfa42c8f8b465fceda341563e89b
Navya <navya.somayajula@toyota.com>
2020-05-08 14:45:43 +0530

    LDNG-4249 Model Year Analytics changes

--- ui.apps/src/main/content/jcr_root/apps/lexusdrivers/clientlibs/clientlib-base/toyotacommon.js
+++ ui.apps/src/main/content/jcr_root/apps/lexusdrivers/clientlibs/clientlib-base/toyotacommon.js
@@ -56,7 +56,8 @@ digitalData = {
         '<tag_id>': "",
         '<zip_code>': "",
         "<registration_type>": "",
-        "<role>": ""
+        "<role>": "",
+        "<owner_model_name>": getOwnerModelName()
     }
 
 }
@@ -102,5 +103,15 @@ $(document).on("click", "[data-firetag]", function () {
         } else TOAnalytics.fire(tagId);
     }
 })
+
+function getOwnerModelName() {
+    var fireTagOwnerModelName = "";
+    fireTagOwnerModelName = $('#nav-bar-selectVehicle-tab').text();
+    if (fireTagOwnerModelName === 'Select a Vehicle') {
+        fireTagOwnerModelName = "";
+    }
+    return fireTagOwnerModelName;
+
+}
 
 console.log('from clientlib');
